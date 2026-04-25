f5becb02733c823a61c893ac1fac48d10ff1de10
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2026-04-25 14:16:42 -0500

    codigo atualizado

--- Api_rest/src/main/java/bancodigital/control/ControlConta.java
+++ Api_rest/src/main/java/bancodigital/control/ControlConta.java
@@ -1,6 +1,7 @@
 package bancodigital.control;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,10 +29,18 @@ public class ControlConta {
         System.out.println("Buscando a conta número: " + dados.getNumeroConta());
         return servicoConta.buscarPorNumero(dados.getNumeroConta());
     }
-     @PostMapping("/buscar-por-id")
-public Conta buscarId(@RequestBody ContaCorrente dados) {
-    System.out.println("Procurando pelo ID: " + dados.getId());
-    return servicoConta.buscarPorId(dados.getId());
-}
-}
 
+    @PostMapping("/buscar-por-id")
+    public Conta buscarId(@RequestBody ContaCorrente dados) {
+        System.out.println("Procurando pelo ID: " + dados.getId());
+        return servicoConta.buscarPorId(dados.getId());
+    }
+    @PostMapping("/transferir")
+    public ResponseEntity<String> realizarTransfer(@RequestBody ContaCorrente dados){
+        boolean sucesso = servicoConta.transferir(dados.getNumeroConta(), dados.getNumeroConta(), dados.getSaldo());
+        if (sucesso) {
+            return ResponseEntity.ok("Transferência realizada com sucesso!");
+        }
+        return ResponseEntity.badRequest().body("Erro ao processar transferência.");
+    }
+}
--- Api_rest/src/main/java/bancodigital/service/ServicoConta.java
+++ Api_rest/src/main/java/bancodigital/service/ServicoConta.java
@@ -16,8 +16,17 @@ public class ServicoConta {
     public Conta criarNovaConta(String titular, String numero, double saldo) {
 
         Conta novaConta = new ContaCorrente(titular, numero, saldo);
+        try {
+            if (contaRepository.findByNumeroConta(numero).isEmpty() && !titular.isEmpty()) {
+                     return contaRepository.save(novaConta);
+                
+            }
+            return null;
 
-        return contaRepository.save(novaConta);
+        } catch (Exception e) {
+            System.out.println("Deu error em salva: "+ e.getMessage());
+            return null;
+        }
     }
 
     public Conta buscarPorId(Long id) {
@@ -27,4 +36,25 @@ public class ServicoConta {
     public Conta buscarPorNumero(String numero) {
         return contaRepository.findByNumeroConta(numero).orElseThrow(() -> new RuntimeException("Erro: Conta " + numero + " não existe no sistema!"));
     }
+    public boolean transferir(String origem, String destino, double valor){
+        // O var é uma palavra-chave introduzida no Java 10 para facilitar a escrita de código, permitindo a Inferência de Tipo de Variável Local.
+        var contaOrigem = contaRepository.findByNumeroConta(origem);
+        var contaDestino = contaRepository.findByNumeroConta(destino);
+
+        if (contaOrigem.isPresent() && contaDestino.isPresent() && valor > 0) {
+            var origemConta = contaOrigem.get();
+            var destinoConta = contaDestino.get();
+            if (origemConta.getSaldo() >= valor) {
+                origemConta.setSaldo(origemConta.getSaldo() - valor);
+                destinoConta.setSaldo(destinoConta.getSaldo() + valor);
+                contaRepository.save(origemConta);
+                contaRepository.save(destinoConta);
+            } else {
+                System.out.println("Saldo insuficiente para transferência.");
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }; 
 }
